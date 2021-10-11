6d1a6734bb4fd37eeea767c615c5f99ddcb53392
Sami Ilahi <63911027+Sami-Ilahi@users.noreply.github.com>
2021-10-11 17:42:19 +0200

    Routes and Controllers steup

---
+++ controllers/bootcamps.js
@@ -0,0 +1,30 @@
+//@desc    Get all bootcamps
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+};
+
+//@desc    Get single bootcamp
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+};
+
+//@desc    Create new bootcamp
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+};
+
+//@desc    Update bootcamp
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+//@desc    Delete bootcamp
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
